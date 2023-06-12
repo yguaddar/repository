8057dae2a9a282fbccf78fbc7458b070f9c7b8dc
yguaddar <136254904+yguaddar@users.noreply.github.com>
2023-06-12 10:06:28 +0200

    Übung 1 java programmig
    New java files are added to the repository

---
+++ Vorlesung.java
@@ -0,0 +1,19 @@
+public class Übungausdervorlesung {
+
+  public static void main(String[] args) {
+    int feld[]=new int[]{
+      5,9,8,9,1,2
+    } ;
+    for(int i=0;i<feld.length;i++){
+      System.out.println(feld[i]);
+    }
+//    for(int a=feld[]{
+//      System.out.println(a);
+//    }
+    char zeichenkette[]=new char[]{
+      'H','A','U','S'
+    };
+    String S=new String(zeichenkette);
+    System.out.println(S);
+  }
+}
---
+++ Ü1A2.java
@@ -0,0 +1,44 @@
+/* Aufgabe 2: Erstellen eines Menüs */
+import java.io.*;
+public class Ü1A2{
+  static int menu()throws IOException{
+   BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+   int wahl;
+   do {
+    System.out.println("1.Oeffnen");
+    System.out.println("2.Schliessen");
+    System.out.println("3.Drucken");
+    System.out.println("4.Neu");
+    System.out.println("5.Beenden");
+    wahl = Integer.parseInt(in.readLine()); /*einlesen*/
+   }while (wahl < 0 || wahl > 5);
+   return wahl;
+  }
+  public static void main(String[] args)throws IOException{
+   boolean weiter =true;
+   do {
+   int auswahl = menu();
+
+   switch (auswahl){
+     case 1 :
+     System.out.println("1.oeffnen");
+     break;
+     case 2 :
+     System.out.println("2.shcliessen");
+     break;
+     case 3 :
+     System.out.println("3.drucken");
+     break;
+     case 4 :
+     System.out.println("4.neu");
+     break;
+     case 5 :
+     System.out.println("5.beenden");
+     System.out.println("ende");
+     weiter = false;
+     break;
+   }
+  } while(weiter);
+}
+
+}
